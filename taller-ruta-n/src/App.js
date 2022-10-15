b0f76e16c4208c7199ac58816d09c68542d0eccf
johnalexdeveolp10 <johnzeta10@gmail.com>
2022-10-15 08:55:53 -0500

    first

--- taller-ruta-n/src/App.js
+++ taller-ruta-n/src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react'
+
 import './App.css';
 
 function App() {
+ const [characters, setCharacters]= useState([])
+
+async function getAllCharacters(){
+const response = await fetch('https://rickandmortyapi.com/api/character')
+const characters = await response.json()
+const alivedCharacters = characters.results.filter(character=>character.status==='Alive')
+setCharacters(alivedCharacters)
+}
+
+function killCharacters(){
+const killCharacters = [...characters]
+killCharacters.forEach(character=>  character.status = 'Alive')
+setCharacters(killCharacters)//asignamos el valor al stado
+
+}
+
+useEffect(()=>{
+getAllCharacters()
+},[])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section className="main">
+     <h1 className="title">Rick y morty</h1>
+     <p className="description">esta es una lista de personajes Rick</p>
+     <button className='button' 
+     onClick={killCharacters}>Matar todos los personajes</button>
+     <section className="grid">
+     {characters?.map(character=>(
+     
+      <div className="card">
+     <img 
+     className="image"
+     src={character.image}
+     alt={character.name}
+     height="200px"
+     width="200px"
+     />
+     
+     <h2 className="name">{character.name}</h2>
+     <div className='info'>
+     <span className={`status ${character.status !== 'alive' ? 'red-status' : ''}`}/>
+     <span className="text">{character.status} - {character.species}</span>
+     </div>
+     </div>
+     
+     
+     ))}
+  
+    
+
+    
+    
+     </section>
+    </section>
+    );
 }
 
 export default App;
+//nota: titulo = estado
+//foreach=no retorna nada
